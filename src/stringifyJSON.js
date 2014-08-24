d5f1855aa0601fc0c96b3300c005fbd5414d7e36
Ralph S <theralphsamuel@gmail.com>
2014-08-24 04:28:26 -0700

    stringifyJSON() processes null, functions, non-nested values, and arrays. Began work on Object handling.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,49 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var resultString = '';
+
+  //Skip functions and 'undefined'
+  if (Object.prototype.toString.call(obj) == "[object Function]" || obj === undefined){
+  	return;
+  }
+
+
+  //Non-nested values
+  //If not Object nor Array
+  if (Object.prototype.toString.call(obj) !== "[object Object]" && !Array.isArray(obj)){
+  	
+  	if(obj === null) {return'null';}
+  	if(typeof obj === 'string') {return'"' + obj.toString() + '"';}
+  	else{return obj.toString();}
+  	
+  }
+
+
+  //Nested values
+  //If Array
+  if(Array.isArray(obj)){
+	resultString += '[';
+	for (var i=0; i<obj.length; i++){
+		resultString += stringifyJSON(obj[i]);
+		if (i < obj.length-1){resultString += ',';}
+	} 
+    resultString +=']';
+  }
+
+  //If Object
+  if (Object.prototype.toString.call(obj) == '[object Object]'){
+	  resultString += '{';
+	  //var keys = Object.keys(obj);
+
+	  for (var prop in obj){
+	  	console.log(prop);
+	  }
+
+	  return resultString + '}';
+  }
+
+  return resultString;
 };
+
